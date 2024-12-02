15d56b9b2f7cdba392d823d953f9effebdfe3132
KesiaRocha <124710521+KesiaRocha@users.noreply.github.com>
2024-12-02 14:41:18 -0300

    Add files via upload

---
+++ timeline/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.period').forEach(period => {
+    period.addEventListener('mouseenter', () => {
+      const box = period.querySelector('.box');
+      box.textContent = period.getAttribute('data-info');
+    });
+  });
+  
